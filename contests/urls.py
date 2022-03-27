981698b4e9e50576aa2579f72cf503785909ca1c
Ilia <smorodin.it@gmail.com>
2022-03-27 08:59:15 +0300

    dev

--- contests/urls.py
+++ contests/urls.py
@@ -24,10 +24,4 @@ story_urlpatterns = [
     # Base CRUD
     path("list/", StoryListView.as_view()),
     path("retrieve/<str:uuid>", StoryRetrieveView.as_view()),
-    # path("create/", ContestCreateView.as_view()),
-    # path("retrieve/<str:uuid>/", ContestRetrieveView.as_view()),
-    # path("update/<str:uuid>/", ContestUpdateDestroyView.as_view()),
-    # path("delete/<str:uuid>/", ContestUpdateDestroyView.as_view()),
-    # Toggle publish
-    # path("toggle-publish/<str:uuid>/", ContestTogglePublish.as_view()),
 ]
